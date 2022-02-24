aae1933c17b18b500b5aa0e3cc297c984023c0ff
tamaron <20992019+tamaroning@users.noreply.github.com>
2022-02-24 11:17:28 +0900

    Update bof1.c

--- bof1.c
+++ bof1.c
@@ -5,17 +5,14 @@ char buffer[32];
 
 void vuln(){
     char local[32];
-
     printf("buffer : 0x%x\n", &buffer);
-
-    fgets(local, 128, stdin);
-    strcpy(buffer, local);
     
+    fgets(local, 128, stdin);
+    strcpy(buffer, local);    
     return;
 }
 
 int main(){
     vuln();
-
     return 0;
 }
